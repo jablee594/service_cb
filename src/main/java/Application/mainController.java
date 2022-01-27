f7d93a46f9064a6514261744427e1e1b18b8c661
Ilya Platytsin <jablee594@gmail.com>
2022-01-27 11:57:39 +0700

    adding a handler

--- src/main/java/Application/mainController.java
+++ src/main/java/Application/mainController.java
@@ -1,8 +1,15 @@
 package Application;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class mainController {
 
@@ -13,8 +20,50 @@ public class mainController {
     private URL location;
 
     @FXML
+    public TextField date;
+
+    @FXML
+    public TextField charcode;
+
+    @FXML
     void initialize() {
 
     }
 
+    public void onClickMethod(ActionEvent actionEvent) throws IOException {
+        String getUserDate = date.getText();
+        if (!getUserDate.equals("")) {
+            String output = getUrlContent("http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date);
+
+            if (!output.isEmpty()){
+
+            }
+        }
+    }
+
+    private static String getUrlContent(String urlAddress) throws IOException {
+
+        URL url = new URL(urlAddress);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine = null;
+        StringBuffer content = new StringBuffer();
+
+        while (true) {
+            try {
+                if ((inputLine = in.readLine()) == null) break;
+            } catch (IOException e) {
+                System.out.println("Неверно введенная дата");
+            }
+            content.append(inputLine);
+        }
+        in.close();
+
+        System.out.println(content);
+
+        return content.toString();
+    }
 }
