e7050ebb653e9e9f70baec3a46dc9ddc0ab5c819
Ilya Platytsin <jablee594@gmail.com>
2022-01-31 16:23:50 +0700

    edd controller

--- src/main/java/Application/mainController.java
+++ src/main/java/Application/mainController.java
@@ -3,13 +3,25 @@ package Application;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import java.io.StringReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
 
 public class mainController {
 
@@ -26,44 +38,50 @@ public class mainController {
     public TextField charcode;
 
     @FXML
+    public Text text_output;
+
+    @FXML
     void initialize() {
 
     }
 
-    public void onClickMethod(ActionEvent actionEvent) throws IOException {
+    public void onClickMethod(ActionEvent actionEvent) throws IOException, ParserConfigurationException, XPathExpressionException, SAXException {
         String getUserDate = date.getText();
         if (!getUserDate.equals("")) {
             String output = getUrlContent("http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date);
+            StringBuffer content = new StringBuffer();
 
-            if (!output.isEmpty()){
+            URL url = new URL(urlAddress);
+            URLConnection urlConn = url.openConnection();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+            String line;
+
+
+            String result = null;
+            if (!output.isEmpty()) {
+                NodeList nl = null;
+                result = null;
+
+                DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document doc = builder.parse(new InputSource(new StringReader(content.toString())));
+                XPathFactory xPathfactory = XPathFactory.newInstance();
+                XPath xpath = xPathfactory.newXPath();
+
+                XPathExpression cod = xpath.compile("//ValCurs/Valute[CharCode='" + charcode + "']/Value/text()");
+
+                nl = (NodeList) cod.evaluate(doc, XPathConstants.NODESET);
+
+                for (int i = 0; i < nl.getLength(); i++) {
+                    Node n = nl.item(i);
+                    result = n.getTextContent();
+                }
+
+                text_output.setText("Курс равен: " + result);
+
 
             }
         }
     }
-
-    private static String getUrlContent(String urlAddress) throws IOException {
-
-        URL url = new URL(urlAddress);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        connection.setRequestMethod("GET");
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String inputLine = null;
-        StringBuffer content = new StringBuffer();
-
-        while (true) {
-            try {
-                if ((inputLine = in.readLine()) == null) break;
-            } catch (IOException e) {
-                System.out.println("Неверно введенная дата");
-            }
-            content.append(inputLine);
-        }
-        in.close();
-
-        System.out.println(content);
-
-        return content.toString();
-    }
-}
