b09aefa4a678438e8eca028662ca1a910c369eba
Ilya Platytsin <jablee594@gmail.com>
2022-02-02 15:52:44 +0700

    edd controller, fxml, connectionDB

--- src/main/java/Application/mainController.java
+++ src/main/java/Application/mainController.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -29,6 +30,15 @@ public class mainController {
 
     @FXML
     private ResourceBundle resources;
+
+    @FXML
+    private LineChart<?, ?> chart;
+
+    @FXML
+    private TextField firstdate;
+
+    @FXML
+    private TextField seconddate;
 
     @FXML
     private URL location;
